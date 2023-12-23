3764b2d207c4c6b961c6955985c4ee5e9d3537ec
MyKe01 <mstasi01@gmail.com>
2023-12-23 11:18:39 +0100

    ckb implementation dir

---
+++ ITD/CodeKataBattle/src/main/java/ckb/platform/CodeKataBattleApplication.java
@@ -0,0 +1,13 @@
+package ckb.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeKataBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeKataBattleApplication.class, args);
+	}
+
+}
