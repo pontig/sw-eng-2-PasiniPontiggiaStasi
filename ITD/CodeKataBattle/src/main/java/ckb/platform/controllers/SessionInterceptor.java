b22ea79e9201185ef831873e8840d19f4bba549e
pontig <pontiggiaelia@gmail.com>
2024-01-31 17:55:27 +0100

    fixed little bug about session

---
+++ ITD/CodeKataBattle/src/main/java/ckb/platform/controllers/SessionInterceptor.java
@@ -0,0 +1,22 @@
+package ckb.platform.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.logging.Logger;
+
+public class SessionInterceptor implements HandlerInterceptor {
+    private static final Logger log = Logger.getLogger(SessionInterceptor.class.getName());
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        log.info("SessionInterceptor:" + request.getRequestURI());
+        if (!(request.getRequestURI().contains("login") || request.getRequestURI().contains("index.html")) && session.getAttribute("user") == null) {
+            response.sendRedirect("login");
+            return false;
+        }
+        return true;
+    }
+}
