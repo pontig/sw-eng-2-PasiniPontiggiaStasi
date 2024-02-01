f99dde1bc918b0e4ffb13e5d861c454da4ecc8a6
pontig <pontiggiaelia@gmail.com>
2024-02-01 11:12:18 +0100

    shmol fixes

---
+++ ITD/CodeKataBattle/src/main/java/ckb/platform/formParser/InviteSinglePersonRequest.java
@@ -0,0 +1,14 @@
+package ckb.platform.formParser;
+
+public class InviteSinglePersonRequest {
+    private String email;
+    private Long teamId;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+}
